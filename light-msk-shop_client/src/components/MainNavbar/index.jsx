9d264b967e408478641962e5a313e2879a9c9de5
Jopkins <sergebrew@gmail.com>
2022-08-15 19:33:00 +0300

    headers design rework

--- light-msk-shop_client/src/components/MainNavbar/index.jsx
+++ light-msk-shop_client/src/components/MainNavbar/index.jsx
@@ -5,16 +5,16 @@ import whatsapp from "../../assets/img/whatsapp.svg";
 
 const MainNavbar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-light clear">
+    <nav className="navbar navbar-expand-md bg-light clear mt-3">
       <div className="container">
-        <div className="navbar-brand">
-          <Link to='/light-sp'>ЛОГОТИП</Link>
-        </div>
         <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"/>
         </button>
+        <div className="navbar-brand ms-0">
+          <Link to='/light-sp'>ЛОГОТИП</Link>
+        </div>
         <div className="navbar-collapse collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
@@ -32,18 +32,13 @@ const MainNavbar = () => {
                 <Link to='/light-sp/contacts'>Контакты</Link>
               </div>
             </li>
-          </ul>
-          <ul className="navbar-nav ">
             <li className="nav-item">
               <div className="nav-link">
                 <Link to='/light-sp/delivery'>Доставка и оплата</Link>
               </div>
             </li>
-            <li className="nav-item">
-              <div className="nav-link">
-                <b>+7 (999) 999-99-99</b>
-              </div>
-            </li>
+          </ul>
+          <ul className="navbar-nav">
             <li className="nav-item">
               <div className="nav-link">
                 <img width={25} src={instagram} alt=""/>
@@ -54,8 +49,18 @@ const MainNavbar = () => {
                 <img width={25} src={whatsapp} alt=""/>
               </div>
             </li>
+            <li className="nav-item">
+              <div className="nav-link">
+                <b><a href="tel:+79999999999">+7 (999) 999-99-99</a></b>
+              </div>
+            </li>
           </ul>
         </div>
+        <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false"
+                aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"/>
+        </button>
       </div>
     </nav>
   );
--- light-msk-shop_client/src/components/SecondNavbar/index.jsx
+++ light-msk-shop_client/src/components/SecondNavbar/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import bagDash from '../../assets/img/bag-dash.svg';
 
 const SecondNavbar = () => {
   return (
-    <nav className="navbar sticky-top navbar-expand-md navbar-dark bg-black clear">
+    <nav className="navbar sticky-top navbar-expand-md bg-light clear m-2 p-2">
       <div className="container">
-
-        <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent2" aria-expanded="false"
-                aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"/>
-        </button>
-
         <div className="navbar-collapse collapse" id="navbarSupportedContent2">
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            <li className="nav-item">
+              <input className="form-control me-2" type="search" placeholder="Поиск" aria-label="Search"/>
+            </li>
+          </ul>
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
               <div className="nav-link">
@@ -40,15 +39,12 @@ const SecondNavbar = () => {
               </div>
             </li>
           </ul>
-          <ul className="navbar-nav me-auto mb-2 mb-md-0">
-            <li className="nav-item">
-              <input className="form-control me-2" type="search" placeholder="Поиск" aria-label="Search"/>
-            </li>
-          </ul>
           <ul className="navbar-nav me-0">
             <li className="nav-item">
               <div className="nav-link">
-                <Link to='/light-sp/cart'>Корзина</Link>
+                <Link to='/light-sp/cart'>
+                  <img width={25} src={bagDash} alt=""/>
+                </Link>
               </div>
             </li>
           </ul>
